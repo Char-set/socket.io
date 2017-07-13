5257565d700d5d6156ad180895dc2e68eb79020f
Char-set <xcharset@163.com>
2017-07-13 13:05:58 +0800

    Update _app.js
    update _app.js

--- _app.js
+++ _app.js
@@ -1,16 +1,10 @@
-/*专为其他App提供后端服务，如文件存储服务（qiniu）
- */
 var _app = global._app = {};
 _app.hostPort = 8000;
 
 //外部库引入
 var $http = global.$http = require('http');
 var $https = global.$https = require('https');
-var $fs = global.$fs = require('fs');
-var $path = global.$path = require('path');
-var $co = global.$co = require('co');
 var $koa = global.$koa = require('koa');
-var $crypto = global.$crypto = require('crypto');
 var $socketio = global.$socketio = require('socket.io');
 
 //自定义库引入
@@ -21,7 +15,6 @@ var httpSvr = _app.httpSvr = $http.createServer(koaSvr.callback());
 
 //启动socketio服务
 var sktSvr = _app.sktSvr = $socketio(httpSvr);
-
 
 var allskts = {};
 sktSvr.on('connection', function (skt) {
@@ -34,7 +27,6 @@ sktSvr.on('connection', function (skt) {
 
 	skt.emit('chat', __newMsg(1, 'ok', msg1));
 
-
 	skt.on('chat', function (msg) {
 		for (var attr in allskts) {
 			var sktcli = allskts[attr];
@@ -43,14 +35,9 @@ sktSvr.on('connection', function (skt) {
 	});
 });
 
-
-//读取外部xcfg文件写入_xfg全局参数
 (function () {
 	_app.httpSvr.listen(_app.hostPort);
 	console.info('Server is running on:' + _app.hostPort + '!');
 })();
-
-
-
 
 koaSvr.use(require('koa-static')('www'));
